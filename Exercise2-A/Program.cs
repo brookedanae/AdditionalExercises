aab0ce638de344a93225d6975cf5111361ba3760
brooke <brookedanae88@gmail.com>
2021-02-22 00:31:04 -0500

    Initial commit

---
+++ Exercise2-A/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise2_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            do
+            {
+                Console.WriteLine("Hello! Please choose between English, Spanish, or Dutch! Enter a language:");
+                var userInput = Console.ReadLine().ToLower();
+
+                switch (userInput)
+                {
+                    case "english":
+                        Console.WriteLine("Hello, World!");
+                        break;
+                    case "spanish":
+                        Console.WriteLine("Hola Mundo!");
+                        break;
+                    case "dutch":
+                        Console.WriteLine("Hallo wereld!");
+                        break;
+                    default:
+                        Console.WriteLine("Not a valid selection!");
+                        break;
+                }
+
+                Console.WriteLine("Would you like to continue? (y/n): ");
+                answer = Console.ReadLine().ToLower();
+            } while (answer == "y");
+
+            Console.WriteLine("Thank you! Goodbye!");
+        }
+    }
+}
---
+++ Exercises1-5/Exercises1-5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exercises1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise 1
+            Console.Write("Hey! What's your name? ");
+            var userName = Console.ReadLine();
+            Console.WriteLine($"What's up, {userName}!" + "\n");
+
+            // Exercise 2
+            Console.Write("Okay, now enter a number! ");
+            var userNum = int.Parse(Console.ReadLine());
+            var userNum2 = userNum + 1;
+            Console.WriteLine($"Thanks, {userName}! Your number WAS {userNum}, but we added one and now it's {userNum2}!" + "\n");
+
+            // Exercise 3
+            Console.Write("This is gonna be weird, but enter another number! ");
+            var userNum3 = int.Parse(Console.ReadLine());
+            var userNum4 = userNum3 + 0.5;
+            Console.WriteLine($"Okay so what had happened was... we took your number, {userNum3}, and added 0.5 to it. So now we have {userNum4}!");
+
+            // Exercise 4
+            Console.Write("\n" + "Now we're gonna try adding two of your numbers together! Enter one number: ");
+            var userNum5 = int.Parse(Console.ReadLine());
+            Console.Write("Got it. Now enter another number: ");
+            var userNum6 = int.Parse(Console.ReadLine());
+            var userSum = userNum5 + userNum6;
+            Console.WriteLine($"Thanks! You entered: {userNum5} & {userNum6}.");
+            Console.WriteLine($"Adding those together, we get {userSum}!");
+
+            // Exercise 5
+            Console.Write("I promise this is the last time I'm gonna ask! We're gonna multiply two of your numbers. Enter a number: ");
+            var userNum7 = int.Parse(Console.ReadLine());
+            Console.Write("Cool. Enter one last number: ");
+            var userNum8 = int.Parse(Console.ReadLine());
+            var userMult = userNum7 * userNum8;
+            Console.WriteLine($"So you entered {userNum7} & {userNum8}...");
+            Console.WriteLine($"Multipled together & we get {userMult}!" + "\n");
+
+            Console.WriteLine("Thanks for playing!");
+        }
+    }
+}
