afcb9ce06b0e93e60f9462510a7b2a92007cae0e
brooke <brookedanae88@gmail.com>
2021-02-22 23:26:33 -0500

    Started NumberGuessingGame, Finished Exercise 4-A

---
+++ Exercise4-A/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise4_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string response;
+            bool isInvalid;
+            do
+            {
+                Console.WriteLine("Hello, World!");
+                do
+                {
+                    Console.WriteLine("Would you like to continue? (y/n): ");
+                    response = Console.ReadLine().ToLower();
+                    isInvalid = response != "y" && response != "n";
+                    if (isInvalid)
+                    {
+                        Console.WriteLine("Please enter a valid selection!");
+                    }
+                } while (isInvalid);
+
+            } while (response == "y");
+            Console.WriteLine("Goodbye!");
+
+        }
+    }
+}
---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Welcome to the Guess a Number Game!");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++" + "\n");
+            Console.WriteLine("I'm thinking of a number between 1 and 100.");
+            Console.WriteLine("Try to guess it, n00b." + "\n");
+
+            int userNumber;
+            string userGuess;
+            do
+            {
+                Console.WriteLine("Enter your lousy guess: ");
+                userGuess = Console.ReadLine();
+                if (!int.TryParse(userGuess, out userNumber))
+                {
+                    Console.WriteLine("I said a NUMBER! Try again...");
+                }
+            } while (true);
+
+            Console.WriteLine("You got it!");
+        }
+
+        //public static double GetDoubleWithinRange(string prompt, double min, double max)
+        //{
+
+        //}
+
+        //public static int GetIntWithinRange(string prompt, int min, int max)
+        //{
+
+        //}
+    }
+}
