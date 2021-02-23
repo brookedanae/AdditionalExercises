c9723b868e986ce374a8764b0ac5e27eb7926c81
brooke <brookedanae88@gmail.com>
2021-02-22 23:34:01 -0500

    Finished Exercise 4-B

---
+++ Exercise4-B/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise4_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string response;
+            do
+            {
+                Console.Write("Enter some text: ");
+                var userInput = Console.ReadLine();
+                Console.WriteLine(userInput + "\n");
+                bool isInvalid;
+                do
+                {
+                    Console.Write("Cool. Would you like to add some more? (y/n): ");
+                    response = Console.ReadLine().ToLower();
+                    isInvalid = response != "y" && response != "n";
+                    if (isInvalid)
+                    {
+                        Console.WriteLine("Please enter a valid response! 'y' or 'n'");
+                    }
+                } while (isInvalid);
+
+            } while (response == "y");
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
